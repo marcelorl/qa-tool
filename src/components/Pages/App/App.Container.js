fc2e1cd36e353577f9d11d9cb14737d6b917a686
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2018-04-08 13:25:28 -0300

    chore: linking App.Container to store

--- src/components/Pages/App/App.Container.js
+++ src/components/Pages/App/App.Container.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { add, remove, sort } from '../../../actions/question'
 import App from '../../Templates/App';
 
 class AppContainer extends Component {
   render () {
     return (
       <App {...this.props} />
-    );
+    )
   }
 }
 
-export default AppContainer;
+const mapStateToProps = state => state
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  add,
+  remove,
+  sort
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppContainer)
